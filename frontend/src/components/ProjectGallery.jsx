1fc3a33b8842aaadae081f342e787283a4d0a684
gordnzhou <gordonzhou223@gmail.com>
2025-05-08 19:39:22 -0700

    improve projects section

--- frontend/src/components/ProjectGallery.jsx
+++ frontend/src/components/ProjectGallery.jsx
@@ -6,44 +6,84 @@ const sortedProjects = [...projects].sort((a, b) =>
     new Date(b.date_meta).getTime() - new Date(a.date_meta).getTime()
 );
 
+const tagBackgrounds = {
+    "Hackathon": "#f2a65a ",
+    "Full Stack": "#4d90a0",
+    "Frontend": "#b475d8",
+    "Emulator": "#d94f4f",
+  };
+
 const ProjectGallery = () => {
-    const [currentProject, setCurrentProject] = useState(projects[0]);
+    const [currentProject, setCurrentProject] = useState(sortedProjects[0]);
 
     return (
-        <div class="project-container">
-            <div class="project-minicard-container">
+        <div class="projects-container">
+            <ProjectCard project={currentProject}/>
+            <div class="project-minicards">
+                <p>sorted by date..</p>
                 {sortedProjects.map((project, i) => (
-                    <ProjectMinicard key={i} project={project} setCurrentProject={setCurrentProject} />
+                    <ProjectMinicard 
+                        key={i} 
+                        project={project} 
+                        currentProject={currentProject}
+                        setCurrentProject={setCurrentProject} 
+                    />
                 ))}
-            </div>
-            <div class="current-project-container">
-                <ProjectCard project={currentProject}/>
             </div>
         </div>
     )
 }
 
 const ProjectCard = ({project}) => {
-    // add link icons for github repo, and website
     return (
         <div class="project-card">
-            <a class="project-link" href={project.repo_link} target="_blank">
-                <h3 class="name">{project.name}</h3>
-                <h4 class="text-stack">{project.tech_stack.reduce(
-                    (acc, cur, i) => acc + cur + (i === project.tech_stack.length - 1 ? "" : " | "),
+            <div class="top">
+                <div class="header">
+                    <h3 class="name">{project.name}</h3>
+                    <h3 class="timeframe">{project.timeframe}</h3>
+                </div>
+                <h4 class="text-tags">{project.tech_tags.reduce(
+                    (acc, cur, i) => acc + cur + (i === project.tech_tags.length - 1 ? "" : " | "),
                     ""
                 )}</h4>
-                <p class="description">{project.description}</p>
-                <p class="hover-text">Visit Project Repository {">>>"}</p>
-            </a>
+            </div>
+
+            <div class="description">
+                {project.description}
+            </div>
+
+            <div class="footer">
+                <h3>Project links:</h3>
+                <div class="project-links">
+                    {   project.repo_link && 
+                        <a class="icon-link" href={project.repo_link} target="_blank" aria-label="GitHub">
+                            <i class="fab fa-github"></i>
+                        </a>
+                    }
+                    {   project.website_link && 
+                        <a class="icon-link" href={project.website_link} target="_blank" aria-label="Website">
+                            <i class="fa fa-globe"></i>
+                        </a>
+                    }
+                </div>
+                <div class="tags-container">
+                    {project.other_tags.map((tag, i) => {
+                        return <p style={{backgroundColor: tagBackgrounds[tag]}} class="tag" key={i}>{tag}</p>
+                    })}
+                </div>
+            </div>
         </div>
     )
 }
 
-const ProjectMinicard = ({project, setCurrentProject}) => {
+const ProjectMinicard = ({project, currentProject, setCurrentProject}) => {
     return (
-        <div class="project-minicard" onClick={() => setCurrentProject(project)}>
+        <div class={"project-minicard " + (project.name === currentProject.name && "selected")} onClick={() => setCurrentProject(project)}>
             <h3 class="name">{project.name}</h3>
+            <h4 class="text-stack">{project.tech_tags.reduce(
+                (acc, cur, i) => (i > 3 ? acc : (i == 3 ? acc + "..." : acc + cur + (i === project.tech_tags.length - 1 ? "" : " | "))),
+                ""
+            )}</h4>
         </div>
     )
 }
